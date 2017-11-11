b45ceb19ffe6726dfd291c154ad26105c5954dff
Matthijs Gielen <github@mwgielen.com>
2017-11-11 18:54:23 +0100

    Fixed requirements.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='jackal',
-      version='0.1.3',
+      version='0.1.4',
       description='Jackal provides a way to store results from hacking tools in a single place.',
       author='Matthijs Gielen',
       author_email='github@mwgielen.com',
       url='https://github.com/mwgielen/jackal/',
       packages=['jackal'],
-      install_requires=['elasticsearch_dsl', 'python-libnmap', 'builtins'],
+      install_requires=['elasticsearch_dsl', 'python-libnmap', 'future'],
       scripts=['bin/jk-status', 'bin/jk-hosts', 'bin/jk-ranges', 'bin/jk-import-nmap', 'bin/jk-filter', 'bin/jk-configure', 'bin/jk-netdiscover'])
