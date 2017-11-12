7d85a795010c9dd0fbe05a615f0dad8908d45559
Matthijs Gielen <github@mwgielen.com>
2017-11-12 22:27:53 +0100

    Fixed some bugs with pipes and printing.

--- jackal/utils.py
+++ jackal/utils.py
@@ -1,3 +1,6 @@
+"""
+    Provides some utility functions to aid printing for usage with pipes.
+"""
 import datetime
 import json
 import sys
@@ -13,14 +16,22 @@ def print_line(text):
     """
         Print the given line to stdout
     """
-    sys.stdout.write(text)
-    if not text.endswith('\n'):
-        sys.stdout.write('\n')
+    try:
+        sys.stdout.write(text)
+        if not text.endswith('\n'):
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+    except IOError:
+        pass
 
 
 def print_json(data):
     """
         Print the given data to stdout.
     """
-    sys.stdout.write(json.dumps(data, default=datetime_handler))
-    sys.stdout.write('\n')
+    try:
+        sys.stdout.write(json.dumps(data, default=datetime_handler))
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+    except IOError:
+        pass
