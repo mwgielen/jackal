0dfc3ac2ff4e3297fbda68b5232463b41da02e36
Matthijs Gielen <github@mwgielen.com>
2017-11-16 16:44:29 +0100

    Moved import_nmap to utils.

--- jackal/scripts/import_nmap.py
+++ jackal/scripts/import_nmap.py
@@ -1,37 +1,13 @@
 #!/usr/bin/env python3
-from jackal import Core, Host
-from jackal.utils import print_notification, print_error, print_success
-from libnmap.parser import NmapParser
+from jackal import Core
+from jackal.utils import import_nmap
 
-
-core = Core(use_pipe=False)
-
-def parse_nmap(input_file):
-    parser = NmapParser()
-    print_notification("Jackal importing file: {}".format(input_file))
-    report = parser.parse_fromfile(input_file)
-    print_notification("Number of hosts in the nmap file: {}".format(report.hosts_total))
-    imported_hosts = 0
-    for nmap_host in report.hosts:
-        if nmap_host.status == 'up' or nmap_host.hostnames:
-            imported_hosts += 1
-            host = Host()
-            host.address = nmap_host.address
-            host.tags = ['nmap', nmap_host.status]
-            if nmap_host.os_fingerprinted:
-                host.os = nmap_host.os_fingerprint
-            host.hostname = nmap_host.hostnames
-            for service in nmap_host.services:
-                host.services.append(service.get_dict())
-            core.merge_host(host)
-    if imported_hosts == 0:
-        print_error("No hosts imported, all hosts' status was unknown or down, only hosts with status 'up' or hostname set are imported.")
-    else:
-        print_success("Import done, hosts imported: {}".format(imported_hosts))
 
 def main():
+    core = Core(use_pipe=False)
     if core.arguments.file:
-        parse_nmap(core.arguments.file)
+        import_nmap(core.arguments.file)
+
 
 if __name__ == '__main__':
-    main()
+   main()
--- jackal/utils.py
+++ jackal/utils.py
@@ -4,7 +4,8 @@
 import datetime
 import json
 import sys
-
+from libnmap.parser import NmapParser
+from jackal import Host, Core
 
 def datetime_handler(x):
     if isinstance(x, datetime.datetime):
@@ -51,3 +52,26 @@ def print_error(string):
         Prints a red [!] before the message
     """
     print_line('\033[91m[!]\033[0m {}'.format(string))
+
+def import_nmap(input_file):
+    """
+        Import the given nmap file, returns the number of imported hosts.
+    """
+    core = Core()
+    parser = NmapParser()
+    report = parser.parse_fromfile(input_file)
+
+    imported_hosts = 0
+    for nmap_host in report.hosts:
+        if nmap_host.status == 'up' or nmap_host.hostnames:
+            imported_hosts += 1
+            host = Host()
+            host.address = nmap_host.address
+            host.tags = ['nmap', nmap_host.status]
+            if nmap_host.os_fingerprinted:
+                host.os = nmap_host.os_fingerprint
+            host.hostname = nmap_host.hostnames
+            for service in nmap_host.services:
+                host.services.append(service.get_dict())
+            core.merge_host(host)
+    return imported_hosts
