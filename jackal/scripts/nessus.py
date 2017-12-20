bf5291d1f075358e4af355aed4f172f9811c45b7
Matthijs Gielen <matthijs.gielen@fox-it.com>
2017-12-20 17:15:57 +0100

    Added nessus script.

---
+++ jackal/scripts/nessus.py
@@ -0,0 +1,85 @@
+import json
+import datetime
+from jackal import HostSearch
+from jackal.config import Config
+import requests
+
+
+class Nessus(object):
+    """
+        Quick nessus class to create and start nessus scans.
+    """
+
+    def __init__(self, access, secret, url, template_name):
+        self.headers = {
+            "X-ApiKeys": "accessKey = {}; secretKey = {};".format(access, secret),
+            "content-type": "application/json"
+        }
+        self.url = url
+        self.template_name = template_name
+
+
+    def get_template_uuid(self):
+        """
+            Retrieves the uuid of the given template name.
+        """
+        response = requests.get(self.url + 'editor/scan/templates', headers=self.headers, verify=False)
+        templates = json.loads(response.text)
+        for template in templates['templates']:
+            if template['name'] == self.template_name:
+                return template['uuid']
+
+
+    def create_scan(self, host_ips):
+        """
+            Creates a scan with the given host ips
+            Returns the scan id of the created object.
+        """
+        now = datetime.datetime.now()
+        data = {
+            "uuid": self.get_template_uuid(),
+            "settings": {
+                "name": "jackal-" + now.strftime("%Y-%m-%d %H:%M"),
+                "text_targets": host_ips
+            }
+        }
+        response = requests.post(self.url + 'scans', data=json.dumps(data), verify=False, headers=self.headers)
+        if response:
+            result = json.loads(response.text)
+            return result['scan']['id']
+
+
+    def start_scan(self, scan_id):
+        """
+            Starts the scan identified by the scan_id.s
+        """
+        requests.post(self.url + 'scans/{}/launch'.format(scan_id), verify=False, headers=self.headers)
+
+
+def main():
+    """
+        This function obtains hosts from core and starts a nessus scan on these hosts.
+        The nessus tag is appended to the host tags.
+    """
+    config = Config()
+    core = HostSearch()
+    hosts = core.get_hosts()
+    host_ips = ",".join([host.address for host in hosts])
+
+    url = config.get('nessus', 'host')
+    access = config.get('nessus', 'access_key')
+    secret = config.get('nessus', 'secret_key')
+    template_name = config.get('nessus', 'template_name')
+
+    nessus = Nessus(access, secret, url, template_name)
+
+    scan_id = nessus.create_scan(host_ips)
+    nessus.start_scan(scan_id)
+
+    for host in hosts:
+        host.add_tag('nessus')
+        core.merge(host)
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='jackal',
-      version='0.3.1',
+      version='0.3.2',
       description='Jackal provides a way to store results from hacking tools in a single place.',
       author='Matthijs Gielen',
       author_email='github@mwgielen.com',
       url='https://github.com/mwgielen/jackal/',
       packages=['jackal', 'jackal.scripts'],
-      install_requires=['elasticsearch_dsl', 'python-libnmap', 'future', 'gevent', 'grequests'],
+      install_requires=['elasticsearch_dsl', 'python-libnmap', 'future', 'gevent', 'grequests', 'requests'],
       entry_points={
           'console_scripts': [
               'jk-status = jackal.scripts.status:main',
@@ -22,5 +22,6 @@ setup(name='jackal',
               'jk-services = jackal.scripts.services:main',
               'jk-overview = jackal.scripts.services:overview',
               'jk-add-tag = jackal.scripts.tags:add_tag',
+              'jk-nessus = jackal.scripts.nessus:main',
           ]
       })
