1f248d0510cb89c801547402abf822b65c1a31d0
Matthijs Gielen <github@mwgielen.com>
2018-01-01 18:47:28 +0100

    Updated format and filter with error handling and default styles.

--- jackal/scripts/filter.py
+++ jackal/scripts/filter.py
@@ -1,33 +1,67 @@
 #!/usr/bin/env python3
 import sys
-from os import isatty
-from jackal.utils import print_line
-import json
+import string
+from jackal.utils import print_line, print_error
+from jackal.core import DocMapper, RangeDoc, HostDoc, ServiceDoc
+
+# from https://gist.github.com/navarroj/7689682
+class PartialFormatter(string.Formatter):
+    def __init__(self, missing='~'):
+        self.missing = missing
+
+    def get_field(self, field_name, args, kwargs):
+        # Handle missing fields
+        try:
+            return super().get_field(field_name, args, kwargs)
+        except (KeyError, AttributeError):
+            return None, field_name
+
+    def format_field(self, value, spec):
+        if value is None:
+            return self.missing
+        else:
+            return super().format_field(value, spec)
+
+fmt = PartialFormatter(missing='')
+
+def format_input(style):
+    doc_mapper = DocMapper()
+    if doc_mapper.is_pipe:
+        for obj in doc_mapper.get_pipe():
+            print_line(fmt.format(style, **obj.to_dict(include_meta=True)))
+    else:
+        print_error("Please use this script with pipes")
 
 
 def filter():
-    is_pipe = not isatty(sys.stdin.fileno())
-
-    if is_pipe:
-        if len(sys.argv) > 1:
-            find = sys.argv[1]
-            for line in sys.stdin:
-                line = json.loads(line)
-                result = line.get(find, '')
-                if isinstance(result, list):
-                    result = '[' + ', '.join([str(x) for x in result]) + ']'
-                print_line(result)
-        else:
-            for line in sys.stdin:
-                print_line(line)
+    if len(sys.argv) > 1:
+        style = '{' + str(sys.argv[1]) + '}'
+        format_input(style)
+    else:
+        print_error("Please provide an argument.")
 
 
 def format():
-    is_pipe = not isatty(sys.stdin.fileno())
-    style = "{address:15} {port:7} {protocol:5} {service:15} {state:10} {banner}"
+    """
+        Formats the output of another tool in the given way.
+        Has default styles for ranges, hosts and services.
+    """
+    service_style = "{address:15} {port:7} {protocol:5} {service:15} {state:10} {banner}"
+    host_style = "{address:15} {tags}"
+    ranges_style = "{range:18} {tags}"
     if len(sys.argv) > 1:
-        style = sys.argv[1]
-    if is_pipe:
-        for line in sys.stdin:
-            line = json.loads(line)
-            print_line(style.format(**line))
+        format_input(sys.argv[1])
+    else:
+        doc_mapper = DocMapper()
+        if doc_mapper.is_pipe:
+            for obj in doc_mapper.get_pipe():
+                style = ''
+                if isinstance(obj, RangeDoc):
+                    style = ranges_style
+                elif isinstance(obj, HostDoc):
+                    style = host_style
+                elif isinstance(obj, ServiceDoc):
+                    style = service_style
+                print_line(fmt.format(style, **obj.to_dict(include_meta=True)))
+        else:
+            print_error("Please use this script with pipes")
