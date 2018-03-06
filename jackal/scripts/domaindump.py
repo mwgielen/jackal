affa404d4cad83c8139a400e2aa94ca0568f4076
Matthijs Gielen <matthijs.gielen@fox-it.com>
2018-03-06 16:28:34 +0100

    Fix for domaindump import.

--- jackal/scripts/domaindump.py
+++ jackal/scripts/domaindump.py
@@ -50,7 +50,10 @@ def parse_single_computer(entry):
     """
     computer = Computer(dns_hostname=get_field(entry, 'dNSHostName'), description=get_field(
         entry, 'description'), os=get_field(entry, 'operatingSystem'), group_id=get_field(entry, 'primaryGroupID'))
-    if computer.dns_hostname:
+    ip = get_field(entry, 'IPv4')
+    if ip:
+        computer.ip = ip
+    elif computer.dns_hostname:
         computer.ip = resolve_ip(computer.dns_hostname)
     return computer
 
