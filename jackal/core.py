cbe9c6cec799c058154a1ae4756d7be5796e5d4e
Matthijs Gielen <matthijs.gielen@fox-it.com>
2018-05-25 10:40:15 +0200

    Get domains function added to users.

--- jackal/core.py
+++ jackal/core.py
@@ -430,6 +430,16 @@ class UserSearch(CoreSearch):
         return core_parser
 
 
+    def get_domains(self):
+        """
+            Retrieves the domains of the users from elastic.
+        """
+        search = User.search()
+        search.aggs.bucket('domains', 'terms', field='domain', order={'_count': 'desc'}, size=100)
+        response = search.execute()
+        return [entry.key for entry in response.aggregations.domains.buckets]
+
+
 class CredentialSearch(CoreSearch):
     """
         Class to provide search functionality for credentials.
