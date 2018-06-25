d9fdb4bf3a6a91aa74e445d7cb50b31d18136185
Matthijs Gielen <matthijs.gielen@fox-it.com>
2018-06-25 14:58:01 +0200

    Fixed dependency issue for http-header scan.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jackal',
-      version='0.7.4',
+      version='0.7.5',
       description='Jackal provides a way to store results from hacking tools in a single place.',
       author='Matthijs Gielen',
       author_email='github@mwgielen.com',
@@ -14,7 +14,7 @@ setup(name='jackal',
       requires_python='>=3',
       url='https://github.com/mwgielen/jackal/',
       packages=['jackal', 'jackal.scripts'],
-      install_requires=['elasticsearch_dsl', 'python-libnmap', 'gevent', 'grequests', 'requests', 'dnspython', 'psutil', 'pyinotify', 'ldap3'],
+      install_requires=['elasticsearch_dsl', 'python-libnmap', 'gevent', 'grequests', 'requests', 'dnspython', 'psutil', 'pyinotify', 'ldap3', 'pyopenssl'],
       zip_safe=False,
       entry_points={
           'console_scripts': [
