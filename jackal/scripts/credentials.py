c603e44a068195307b5c6183fff4141e9eede104
Matthijs Gielen <matthijs.gielen@fox-it.com>
2018-06-25 14:55:44 +0200

    Added unique host count to creds-overview.

--- jackal/scripts/credentials.py
+++ jackal/scripts/credentials.py
@@ -25,18 +25,19 @@ def overview():
         Provides an overview of the duplicate credentials.
     """
     search = Credential.search()
-    search.aggs.bucket('password_count', 'terms', field='secret', order={'_count': 'desc'}, size=10)\
+    search.aggs.bucket('password_count', 'terms', field='secret', order={'_count': 'desc'}, size=20)\
         .metric('username_count', 'cardinality', field='username') \
+        .metric('host_count', 'cardinality', field='host_ip') \
         .metric('top_hits', 'top_hits', docvalue_fields=['username'], size=100)
     response = search.execute()
-    print_line("{0:65} {1:5} {2:5} {3}".format("Secret", "Count", "Users", "Usernames"))
+    print_line("{0:65} {1:5} {2:5} {3:5} {4}".format("Secret", "Count", "Hosts", "Users", "Usernames"))
     print_line("-"*100)
     for entry in response.aggregations.password_count.buckets:
         usernames = []
         for creds in entry.top_hits:
             usernames.append(creds.username[0])
         usernames = list(set(usernames))
-        print_line("{0:65} {1:5} {2:5} {3}".format(entry.key, entry.doc_count, entry.username_count.value, usernames))
+        print_line("{0:65} {1:5} {2:5} {3:5} {4}".format(entry.key, entry.doc_count, entry.host_count.value, entry.username_count.value, usernames))
 
 
 if __name__ == '__main__':
